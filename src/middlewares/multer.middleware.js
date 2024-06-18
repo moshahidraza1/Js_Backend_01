0e04ff4a5ec8f4195c0ff8a7d78931254b353928
moshahidraza1 <codinghandle@gmail.com>
2024-06-19 02:11:11 +0530

    connection setup of cloudinary for uploads and multer middleware

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,18 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        // read about null in callback(cb)
+        cb(null,'./public/temp')
+    },
+    filename: function(req,file,cb){
+        // Todo: handle the situation when user uploads more than one file with same name
+        cb(null, file.originalname)
+    }
+})
+
+export const upload = multer(
+    {
+        storage,
+    }
+)
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from "cloudinary"
+// fs -> file system
+import fs from "fs"
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try{
+        if(!localFilePath) return null
+        // upload files on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        //file uploades successfully
+        // Todo: explore reponse outputs
+        console.log("file is successfully uploaded on cloudinary ", response.url);
+        return response;
+    }
+    catch(err){
+        // since file upload failed remove the locally saved file
+        fs.unlinkSync(localFilePath)
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
