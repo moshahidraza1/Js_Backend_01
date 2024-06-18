e40dea0bc0d5b2f9a92d54dc2b1e270fbd992bae
moshahidraza1 <codinghandle@gmail.com>
2024-06-19 00:42:38 +0530

    added user and video models with jwt and bcrypt hashing

---
+++ src/models/video.model.js
@@ -0,0 +1,41 @@
+import mongoose, {Schema, plugin} from "mongoose"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+const videoSchema = new Schema(
+    {
+        videoFile:{
+            type: String, // cloudinary link
+            required: true
+        },
+        thumbnail:{
+            type: String, //cloudinary link
+            required: true
+        },
+        title:{
+            type: String,
+            required: true
+        },
+        owner:{
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        description:{
+            type: String,
+            required: true
+        },
+        views:{
+            type: Number,
+            default: 0
+        },
+        isPublished:{
+            type: Boolean,
+            required: true
+        },
+
+    },
+    {timestamps: true}
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video", videoSchema)
