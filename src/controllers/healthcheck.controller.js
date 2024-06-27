9c85a9fdc56f1bdf0c5c894890c317c4dec5ce6c
moshahidraza1 <codinghandle@gmail.com>
2024-06-27 14:45:48 +0530

    feat: Add health check controller for API status monitoring

---
+++ src/controllers/healthcheck.controller.js
@@ -0,0 +1,12 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse";
+
+const healthCheck = asyncHandler(async(req,res)=>{
+    res.status(200)
+    .json(new ApiResponse(200,{}, " OK Everything is working fine "))
+})
+
+export {
+    healthCheck
+}
