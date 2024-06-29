b3251d715847c7a1256ee4eeda7d1dcd2bb25a3c
moshahidraza1 <codinghandle@gmail.com>
2024-06-29 21:56:54 +0530

    feat: Add playlist and subscription routes and controllers

--- src/app.js
+++ src/app.js
@@ -24,6 +24,9 @@ import commentRouter from "./routes/comment.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
 import healthheckRouter from "./routes/healthcheck.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+
 
 //routes declaration
 app.use("/api/v1/users", userRouter)
@@ -33,6 +36,7 @@ app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/healthcheck", healthheckRouter)
-
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
 
 export { app }
